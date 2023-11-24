12a15665c955781c3ab30a6c3b71c789a3c8406a
ayselhasanli <aysiiihasan@gmail.com>
2023-11-24 16:32:42 +0400

    filter functions added buttons

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
-var allButton = document.getElementById("allProducts")
 var productContainer = document.getElementById("productContainer")
-var breakfastButton = document.getElementById("breakfast")
-var buttonContainer = document.getElementById("buttonContainer")
-var products
+var products = []
 
-allButton.onclick = allProducts
-
-window.onload = async function fetchData ( ) {
+window.onload = async function fetchData () {
     try {
         const response = await fetch('http://localhost:3000/menu');
         const data = await response.json();
@@ -14,13 +9,13 @@ window.onload = async function fetchData ( ) {
     }catch (error) {
         console.error(error);
     }
-    allProducts()
+    showProducts("all")
 }
 
-
-function allProducts() {
+function showProducts(category) {
     let displaySection = products.map((product) => {
-        return `
+        if (product.category === category || category === "all") {
+            return `
             <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
             <div class="product-card row">
                 <div class="col-xl-5 col-lg-5 col-md-4 col-sm-5 col-xs-5">
@@ -28,9 +23,9 @@ function allProducts() {
                 </div>
                 <div class="col-xl-7 col-lg-7 col-md-8 col-sm-7 col-xs-7">
                     <div class="product-info">
-                        <div class="header">
-                            <h4>${product.title}</h4>
-                            <h4 class="price">${product.price}</h4>
+                        <div class="header ">
+                           <h4>${product.title}</h4>  
+                           <h4 class="price">${product.price}</h4>
                         </div>
                     </div>
                     <div class="product-text">
@@ -40,33 +35,11 @@ function allProducts() {
             </div>
         </div>
             `
+        }
     })
 
-    let displayButton = products.map((product) => {
-        return `
-            <ul>
-            <li>
-                <button id="allProducts" >All</button>
-            </li>
-            <li>
-                <button id="breakfast">Breakfast</button>
-            </li>
-            <li>
-                <button>Lunch</button>
-            </li>
-            <li>
-                <button>Shakes</button>
-            </li>
-            <li>
-                <button>Dinner</button>
-            </li>
-        </ul>
-            `
-    })
     displaySection = displaySection.join("");
     productContainer.innerHTML = displaySection;
-    allButton.classList.add("active-button")
 }
-
 
 
